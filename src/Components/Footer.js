e7e2fd269d1cca3612b979169d0c8790986dbcd7
Anjana Bajaj <bajajanjana1999@gmail.com>
2021-07-03 15:52:26 +0530

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+    let myStyle={
+        width:"100%",
+        height:"8vh",
+    }
+    return (
+        <div className="bg-light text-center mt-5"style={myStyle}>
+            This is footer!
+        </div>
+    )
+}
