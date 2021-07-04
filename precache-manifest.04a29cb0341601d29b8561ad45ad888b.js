614a00bb35e044ef12c00f169315b26ef2d05b64
Anjana Bajaj <bajajanjana1999@gmail.com>
2021-07-04 18:13:10 +0530

    Updates

---
+++ precache-manifest.04a29cb0341601d29b8561ad45ad888b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "df54651585518c353f36",
+    "url": "/React_carousel/static/css/main.8bf2f8ae.chunk.css"
+  },
+  {
+    "revision": "df54651585518c353f36",
+    "url": "/React_carousel/static/js/main.5254309a.chunk.js"
+  },
+  {
+    "revision": "3e90b149e1b9bf6a9254",
+    "url": "/React_carousel/static/js/runtime~main.6095d407.js"
+  },
+  {
+    "revision": "82a84e3ffd4481ec945a",
+    "url": "/React_carousel/static/css/2.3a022a21.chunk.css"
+  },
+  {
+    "revision": "82a84e3ffd4481ec945a",
+    "url": "/React_carousel/static/js/2.c92e48ce.chunk.js"
+  },
+  {
+    "revision": "8fe6dbce21ede40cd2f9",
+    "url": "/React_carousel/static/js/3.2dc548b3.chunk.js"
+  },
+  {
+    "revision": "bc04a9e7f46cad94f31dfbeb0e8b8c96",
+    "url": "/React_carousel/index.html"
+  }
+];
